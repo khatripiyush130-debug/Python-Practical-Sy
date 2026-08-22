fcb214ad6be49ce77623f02b1a3c2872823beb4f
khatripiyush130-debug <khatripiyush130@gmail.com>
2026-08-22 10:14:03 +0530

    Update String Analysis.py

--- String Analysis.py
+++ String Analysis.py
@@ -1,21 +1,23 @@
-paragraph = str(input("Enter the Paragraph = "))
+print("******string Analysis*******")
+paragraph= input("Enter a paragraph :- ")
 
-char_count = 0
+vowels = "aeiouAEIOU"
 vowel_count = 0
-space_count = 0
+char_count= 0
+space_count= 0
 
-vowel = "aeiouAEIOU"
+for ch in paragraph:
+   if ch == " ":
+      space_count+=1
 
-for i in paragraph:
-    char_count = char_count + 1
+   if ch in vowels:
+      vowel_count+=1
 
-    if i in vowel:
-        vowel_count = vowel_count + 1
+if ch=="":
+   word_count=0
+else:
+   word_count = space_count + 1
 
-    elif i == " ":
-        space_count = space_count + 1
-
-print("Total Characters =", char_count)
-print("Total Vowels =", vowel_count)
-
-print("Total Spaces =", space_count)
+print("vowel Count=", vowel_count)
+print("word Count=", word_count)
+print("space Count=", space_count)
